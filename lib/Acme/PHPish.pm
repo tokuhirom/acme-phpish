ae85883058602724fbf0eb0765857261bc19df9c
Tokuhiro Matsuno <tokuhirom@gp.ath.cx>
2008-12-19 00:46:48 +0900

    initial commit

---
+++ lib/Acme/PHPish.pm
@@ -0,0 +1,33 @@
+package Acme::PHPish;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::PHPish -
+
+=head1 SYNOPSIS
+
+  use Acme::PHPish;
+
+=head1 DESCRIPTION
+
+Acme::PHPish is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom jsdfkla gmail fsadkjl comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
